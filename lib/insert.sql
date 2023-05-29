072f2989b77f31deeaaa240b4ac9264913d03ba3
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-05-29 09:35:19 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "Dark Brown", "Calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "Black", "Intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "Dark Brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "Dark Brown", "Goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "Naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "Black", "Aggressive", 0);
--- lib/seed.sql
+++ lib/seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(1,"Mr. Chocolate", 20, "M", "Dark Brown", "Calm", 0),
+(2,"Rowdy", 10, "M", "Black", "Intense", 1),
+(3,"Tabitha", 6, "F", "Dark Brown", "Nice", 1),
 (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(5,"Melissa", 13, "F", "Dark Brown", "Goofy", 1),
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+(7,"Wendy", 6, "F", "Blue", "Naive", 1),
+(8,null, 20, "M", "Black", "Aggressive", 0);
